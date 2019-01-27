975e2be59b9da9a4e58248dffbdea3cec4b47de3
Henry Weickert <henry.weickert>
2019-01-27 14:21:23 +0100

    make `node_matches_type` accept subclassed nodes as well

--- fstree/_shared.py
+++ fstree/_shared.py
@@ -3,11 +3,14 @@ TYPE_FILE = 'file'
 TYPE_DIR = 'dir'
 
 def node_matches_type(node, type):
+    from . _FileNode import FileNode
+    from . _DirNode import DirNode
+
     if type == TYPE_ALL:
         return True
-    elif type == TYPE_DIR and node.__class__.__name__ == 'DirNode':
+    elif type == TYPE_DIR and isinstance(node, DirNode):
         return True
-    elif type == TYPE_FILE and node.__class__.__name__ == 'FileNode':
+    elif type == TYPE_FILE and isinstance(node, FileNode):
         return True
     return False
 
