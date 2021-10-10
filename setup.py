289ebcaa729e8fe7e1e5da0a26abb7242719993b
Henry Weickert <henry.weickert>
2021-10-10 17:30:51 +0200

    Bump version: "1.0.4" → "1.0.5"

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ description = r'''A tree structure to store files & directories.'''
 setuptools.setup(
     name='fstree',
     packages = setuptools.find_packages(exclude=['tests*']),
-    version="1.0.4",
+    version="1.0.5",
     description=description,
     author = 'Henry Weickert',
     author_email = 'henryweickert@gmail.com',
